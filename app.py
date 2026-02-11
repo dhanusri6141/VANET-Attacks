32528d2ca2b5a893fc03d55023c5375db469fdf2
unknown <dhanusrisingamsetty@gmail.com>
2026-02-11 21:39:20 +0530

    fixed port for render

--- app.py
+++ app.py
@@ -291,5 +291,9 @@ def predict_single_submit():
 
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
